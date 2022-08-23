653cea7d5465bdeaefbcb41648f47cbbc00e8730
Gaelle D <gaelle.derambure@genapi.fr>
2022-08-23 15:09:42 +0200

    installation jquery

---
+++ wwwroot/js/index.js
@@ -0,0 +1,12 @@
+﻿var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
+    console.log("Buying item");
+});
+
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
