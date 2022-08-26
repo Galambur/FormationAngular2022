4adeb6bd204dcccdb517e451addd5c7c94aace1b
Gaelle D <gaelle.derambure@genapi.fr>
2022-08-26 09:48:25 +0200

    added login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DutchTreat.Controllers
 {
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger,
+            SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            this._signInManager = signInManager;
         }
 
         public IActionResult Login()
@@ -18,6 +26,31 @@ namespace DutchTreat.Controllers
             if (User.Identity.IsAuthenticated)
                 return RedirectToAction("Index", "App");
 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username, 
+                    model.Password, 
+                    model.RememberMe, 
+                    false);
+
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                        Redirect(Request.Query["ReturnUrl"].First());
+
+                    RedirectToAction("Shop", "App");
+                }
+            }
+
+            ModelState.AddModelError("", "Failed to login");
+
+            // retourn a la page login
             return View();
         }
     }
